a54c48f77dcaf1a554c68fb4bc9391ebbfc5a115
ToM <tomaszlaczek@hotmail.com>
2020-04-24 17:18:14 +0200

    Animmations
    Added smooth slide-fade animmations

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <NavigationBar class="my-2" />
     <v-content style="" class="pt-4 mb-4">
       <v-container>
-        <router-view/>
+        <transition name="slide-fade" mode="out-in">
+          <router-view/>
+        </transition>
       </v-container>
     </v-content>
     <FooterBar/>
@@ -25,3 +27,18 @@ export default {
   })
 };
 </script>
+<style>
+  /***TRANSITION***/
+  .slide-fade-enter{
+    transform: translateX(10px);
+    opacity: 0;
+  }
+  .slide-fade-enter-active,
+  .slide-fade-leave-active{
+    transition: all 0.2s ease;
+  }
+  .slide-fade-leave-to{
+    transform: translateX(-10px);
+    opacity: 0;
+  }
+</style>
